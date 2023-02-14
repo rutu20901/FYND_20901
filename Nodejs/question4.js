07452dbfa3bf0c1c8de6f9c2a9d4543d69c1ffdf
rutu20901 <rutujachavan20901@gmail.com>
2023-02-14 20:50:28 +0530

    Commit

---
+++ Nodejs/question4.js
@@ -0,0 +1,14 @@
+var a = require("mathematics");
+function evenOdd(n){
+    if((n % 2) === 0){
+        console.log("Even");
+    }
+    else{
+        console.log("Odd");
+    } 
+}
+
+evenOdd(2);
+evenOdd(5);
+
+
