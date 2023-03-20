5d6e980d183589f0d8e537bbf1cd630b609cf736
rutu20901 <rutujachavan20901@gmail.com>
2023-03-20 20:50:12 +0530

    Commit

---
+++ VueJS/vueApp/simplevue/src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+    msg:{
+        type:String,
+        required:true,
+    },
+});
+</script>
+
+<template>
+    <div class="container">
+        <h3>{{ msg }}</h3>
+        <h2>Services</h2>
+        <div>
+            <h3>Web developement</h3>
+            <h3>Digital Marketing</h3>
+            <h3>UI/UX Design</h3>
+            <h3>Training</h3>
+        </div>
+
+    </div>
+</template>
